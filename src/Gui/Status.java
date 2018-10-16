5fd5a24eecf2f960533ddcd31aec313839d31967
vansewi <1094020874@qq.com>
2018-10-16 21:48:48 +0800

    Default Changelist

---
+++ src/Gui/Status.java
@@ -0,0 +1,29 @@
+package Gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static Gui.Login.userss;
+
+public class Status extends JPanel{
+    private JLabel user ;
+    private JLabel School;
+    private JLabel Version;
+    public Status(){
+        gui();
+    }
+    public void gui(){
+        user = new JLabel("用户:     "+userss);
+        School = new JLabel("学校:"+"       吉林大学");
+        Version = new JLabel("版本:"+"        1.0");
+        this.setLayout(new GridLayout(1,3));
+        add(user);
+        add(School);
+        add(Version);
+
+
+
+
+
+    }
+}
